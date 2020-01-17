643650973d57168a24ceb09ddbd786fa5154cee0
skkAYE <37926137+skkAYE@users.noreply.github.com>
2020-01-17 15:35:44 +0100

    update end of week

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,24 @@
-(function($) {
+(function ($) {
 
-	"use strict";
+    "use strict";
 
-	var fullHeight = function() {
+    var fullHeight = function () {
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
 
-	};
-	fullHeight();
+    };
+    fullHeight();
 
-	$('#sidebarCollapse').on('click', function () {
-      $('#sidebar').toggleClass('active');
-  });
-    
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
     $('#menuCollapse').on('click', function () {
-      $('#navbarSupportedContent').toggleClass('show');
-  });
+        $('#navbarSupportedContent').toggleClass('show');
+    });
 })(jQuery);
 
 /* 
@@ -26,47 +26,20 @@
  * ci dessous rajouté pour le tableau
  *
  *
- */
-
-$(document).ready(function(){
-    $('.filterable .btn-filter').click(function(){
-        var $panel = $(this).parents('.filterable'),
-        $filters = $panel.find('.filters input'),
-        $tbody = $panel.find('.table tbody');
-        if ($filters.prop('disabled') == true) {
-            $filters.prop('disabled', false);
-            $filters.first().focus();
-        } else {
-            $filters.val('').prop('disabled', true);
-            $tbody.find('.no-result').remove();
-            $tbody.find('tr').show();
-        }
-    });
-
-    $('.filterable .filters input').keyup(function(e){
-        /* Ignore tab key */
-        var code = e.keyCode || e.which;
-        if (code == '9') return;
-        /* Useful DOM data and selectors */
-        var $input = $(this),
-        inputContent = $input.val().toLowerCase(),
-        $panel = $input.parents('.filterable'),
-        column = $panel.find('.filters th').index($input.parents('th')),
-        $table = $panel.find('.table'),
-        $rows = $table.find('tbody tr');
-        /* Dirtiest filter function ever ;) */
-        var $filteredRows = $rows.filter(function(){
-            var value = $(this).find('td').eq(column).text().toLowerCase();
-            return value.indexOf(inputContent) === -1;
-        });
-        /* Clean previous no-result if exist */
-        $table.find('tbody .no-result').remove();
-        /* Show all rows, hide filtered ones (never do that outside of a demo ! xD) */
-        $rows.show();
-        $filteredRows.hide();
-        /* Prepend no-result row if all rows are filtered */
-        if ($filteredRows.length === $rows.length) {
-            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">No result found</td></tr>'));
-        }
-    });
+ 
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+*/
+$(document).on('show.bs.modal', '#suppressionModal', function (event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('name')
+    var modal = $(this)
+    modal.find('.modal-body #name').text(recipient)
 });
--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,23 @@ var router = express.Router();
 
 router.use(express.static(__dirname + '/public'));
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.render('home.ejs', {});
 });
 
-router.get('/streaming', function(req, res) {
+router.get('/streaming', function (req, res) {
     res.render('streaming.ejs', {});
 });
 
-router.get('/logs', function(req, res) {
+router.get('/logs', function (req, res) {
     res.render('logs.ejs', {});
 });
 
-router.get('/repertoire', function(req, res) {
+router.get('/repertoire', function (req, res) {
     res.render('repertoire.ejs', {});
 });
 
-router.get('/contact', function(req, res) {
+router.get('/contact', function (req, res) {
     res.render('contact.ejs', {});
 });
 
