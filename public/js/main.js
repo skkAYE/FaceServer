249304bd1f9347e7afa11bb8e3821d3f1b031650
skkAYE <37926137+skkAYE@users.noreply.github.com>
2020-05-08 13:59:41 +0200

    update

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+
+(function ($) {
+
+    "use strict";
+
+    var fullHeight = function () {
+
+        $('.js-fullheight').css('height', $(window).height());
+        $(window).resize(function () {
+            $('.js-fullheight').css('height', $(window).height());
+        });
+
+    };
+    fullHeight();
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+    $('#menuCollapse').on('click', function () {
+        $('#navbarSupportedContent').toggleClass('show');
+    });
+})(jQuery);
+
+/* 
+ *
+ * ci dessous rajouté pour le tableau
+ *
+ *
+ 
+$('#exampleModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this)
+  modal.find('.modal-title').text('New message to ' + recipient)
+  modal.find('.modal-body input').val(recipient)
+})
+*/
+$(document).on('show.bs.modal', '#suppressionModal', function (event) {
+    var button = $(event.relatedTarget)
+    var recipient = button.data('name')
+    var modal = $(this)
+    modal.find('.modal-body #name').text(recipient)
+});
+
+
+window.addEventListener('load', function() {
+  document.querySelector('input[type="file"]').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+          var img = document.querySelector('img');  // $('img')[0]
+          img.src = URL.createObjectURL(this.files[0]); // set src to blob url
+          img.onload = imageIsLoaded;
+      }
+  });
+});
+
+
+function imageIsLoaded() { 
+  alert("Image loaded successfully");  
+}
+
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,96 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+const multer = require('multer');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+function format(i){
+    if(i<10){
+        return "0"+i;
+    }
+    return i;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+var storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, __dirname + '/../public/images/')
+        },
+        filename: function (req, file, cb) {
+
+            cb(null, file.originalname)
+        }
+    });
+var upload = multer({ storage: storage })
+
+	
+	
+router.use(express.static(__dirname + '/public'));
+
+router.get('/', function (req, res) {
+    res.render('home.ejs', {});
 });
+
+router.get('/streaming', function (req, res) {
+    res.render('streaming.ejs', {});
+});
+
+router.get('/logs', function (req, res) {
+    res.render('logs.ejs', {});
+});
+
+router.get('/repertoire', function (req, res) {
+    res.render('repertoire.ejs', {});
+});
+
+
+router.get('/contact', function (req, res) {
+    res.render('contact.ejs', {});
+});
+
+router.get('/add', function (req, res) {
+    res.render('add.ejs', {query : req.query});
+});
+
+router.post('/add', upload.single('photo'), function (req, res) {
+    console.log(req.body.person_name);
+    res.redirect('/repertoire');
+});
+
+router.post('/', upload.single('photo'), (req, res) => {
+    res.redirect('/');
+	var oldPath = __dirname + '/../public/images/'+req.file.filename
+    let ts = Date.now(); 
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+    let hours = date_ob.getHours();
+    let mins = date_ob.getMinutes();
+	
+	var newPath = __dirname + '/../public/images/test/'
+	console.log(newPath);
+	fs.access(newPath, function(err) {
+		if (err && err.code === 'ENOENT') {
+		fs.mkdir(newPath, (err) => { 
+		if (err)console.log(err); 
+		} ); //Create dir in case not found
+		}
+		else if(err) console.log(err);
+		});
+	
+	newPath = newPath +format(date) + "-" + format(month) + "-" + year + "_" + hours + "h" + mins +  "_" + req.file.filename;
+	fs.rename(oldPath, newPath, function (err) {
+	if (err) console.log("moving of image failed");
+	})
+});
+
 
 module.exports = router;
